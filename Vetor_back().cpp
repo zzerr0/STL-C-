95dd97f181a06c19a5ddc09b544b0ab45dbc68c6
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-08-22 10:51:21 +0530

    Create Vetor_back().cpp

---
+++ Vetor_back().cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   vector <char> v1 = {'a', 'b', 'c', 'd', 'Shivam'};
+   cout<<"working of v1.back(),the last element "<<v1.back();
+   
+   // the output is m
+   return 0;
+}
